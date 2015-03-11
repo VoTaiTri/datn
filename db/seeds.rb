a752e60e18756512c54b429005a4516c67d15b9e
VoTaiTri <vo.tai.tri@framgia.com>
2015-03-11 16:02:29 +0700

    add seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Lecturer.create!(name:  "Admin User",
+             email: "vtt@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             admin: true)
+
+Lecturer.create!(name:  "Example User",
+             email: "tribeo@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456")
